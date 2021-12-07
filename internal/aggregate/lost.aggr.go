3f60c315368bbbeec35bcd6ada8c23fcfe1f3021
xqbumu <xqbumu@163.com>
2021-12-07 08:40:06 +0800

    fix: time format

--- internal/aggregate/lost.aggr.go
+++ internal/aggregate/lost.aggr.go
@@ -52,7 +52,7 @@ func (u *lostAggr) List(ctx context.Context, opts schema.LostListRequest) ([]*sc
 			Name:         v.Nickname,
 			Babyid:       v.Babyid,
 			Introduction: v.Subject,
-			MissAt:       v.MissedAt.Format("2006 15:04:05"),
+			MissAt:       v.MissedAt.Format("2006-01-02 15:04:05"),
 			MissAddr:     v.MissedAddress,
 		}
 	}
@@ -98,13 +98,13 @@ func (u *lostAggr) GetByID(ctx context.Context, id uint) (*schema.LostDetail, er
 				return "未知"
 			}
 		}(),
-		BirthedAt: item.BirthedAt.Format("2006 15:04:05"),
+		BirthedAt: item.BirthedAt.Format("2006-01-02 15:04:05"),
 		Carousel: []string{
 			item.AvatarURL,
 		},
 
 		// 失踪信息
-		MissAt:     item.MissedAt.Format("2006 15:04:05"),
+		MissAt:     item.MissedAt.Format("2006-01-02 15:04:05"),
 		MissAddr:   item.MissedAddress,
 		MissHeight: item.Height,
 		Character:  item.Characters,
